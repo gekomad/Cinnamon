fe27874eb9169f5f6441301c8fceff3baa38e6e9
geko <geko@geko-HP-Compaq-6005-Pro-SFF-PC.seat.it>
2015-06-18 10:31:02 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -65,16 +65,6 @@ jabba-64_2041         0  0  0  0  0  0  0  0  0  0  0  0    50
 gullydeckel-64_1982   0  0  0  0  0  0  0  0  0  0  0  0 49
 
 
-ratings
-Rank Name                 Elo    +    - games score oppo. draws
-   1 Cinnamon 1.2c          4    6    6  3660   51%    -4   29%
-   2 Cinnamon 1.2b         -4    6    6  3660   49%     4   29%
-
-ratings
-Rank Name                 Elo    +    - games score oppo. draws
-   1 beowulf_2194          250   16   15  1000   68%   108   13%
-   2 Cinnamon 1.2b         112   14   14  1000   51%   108   27%
-   3 Cinnamon 1.2c         108   10   11  2423   50%    67   16%
  */
 
 using namespace _board;
