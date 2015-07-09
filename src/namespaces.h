bb7b1739297d61f7f9fae1f21d37f7afc14bf6eb
gekomad <info@cinnamon.com>
2015-07-09 22:10:51 +0200

    .

--- src/namespaces.h
+++ src/namespaces.h
@@ -31,7 +31,7 @@
 using namespace std;
 namespace _board {
 
-    static const string NAME = "Cinnamon 1.2c-smp.3";
+    static const string NAME = "Cinnamon 1.2c-smp.join";
     static const string STARTPOS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
     typedef unsigned char uchar;
