f6b665804543a29520d366a39b6959aca2466c4c
gekomad <info@cinnamon.com>
2015-09-18 21:39:04 +0200

    .

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,12 +1,15 @@
 make clean
 cd ..
 tar -czf src.tar.gz src
+rm -fr /tmp/deploy/
+mkdir /tmp/deploy/
 
 array=( 192.168.1.100 192.168.1.101 192.168.1.102 192.168.1.106 192.168.1.108 )
 
 for ip in "${array[@]}"
-do
+do	
+	echo $ip
 	scp src.tar.gz  geko@$ip:
-	ssh geko@$ip /home/geko/run_match.sh >/dev/null &
+	ssh geko@$ip /home/geko/run_match.sh >/tmp/deploy/$ip.log &
 done
 exit 0
