9f311795c5b77661fd576f22b116a13e59acfc13
gekomad <info@cinnamon.com>
2015-09-20 21:43:30 +0200

    .

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,5 +1,6 @@
 array=( 192.168.1.100 192.168.1.101 192.168.1.102 192.168.1.106 192.168.1.108 )
 
+make clean;make cinnamon64-modern-INTEL COMP=clang++
 rm -fr /tmp/deploy/
 UUID=$(cat /proc/sys/kernel/random/uuid)
 mkdir -p /tmp/deploy/$UUID
