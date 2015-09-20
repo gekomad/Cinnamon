4cfe81ec5ce644a69c7f300fc88851711e73ff17
gekomad <info@cinnamon.com>
2015-09-20 16:04:58 +0200

    .

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,13 +1,15 @@
 array=( 192.168.1.100 192.168.1.101 192.168.1.102 192.168.1.106 192.168.1.108 )
 
 make clean;make cinnamon-ARM COMP=arm-linux-gnueabihf-g++-4.9
+./cinnamon -b
 rm -fr /tmp/deploy/
 UUID=$(cat /proc/sys/kernel/random/uuid)
 mkdir -p /tmp/deploy/$UUID
 cp -r ../src /tmp/deploy/$UUID
 cd /tmp/deploy
+rm -fr $UUID/src/*.o $UUID/src/gtb/Linux $UUID/src/gtb/Windows $UUID/src/gtb/OSX
 tar -czf $UUID.tar.gz $UUID
-./cinnamon -b
+
 echo "send to servers ? y/n"
 read send
 if [ "$send" != "y" ]; then
