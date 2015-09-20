2ca74c29608d05c7c7bf8858c256969491279590
gekomad <info@cinnamon.com>
2015-09-20 12:53:14 +0200

    .

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,15 +1,21 @@
-make clean
-cd ..
-tar -czf src.tar.gz src
-rm -fr /tmp/deploy/
-mkdir /tmp/deploy/
-
 array=( 192.168.1.100 192.168.1.101 192.168.1.102 192.168.1.106 192.168.1.108 )
+
+make clean
+rm -fr /tmp/deploy/
+UUID=$(cat /proc/sys/kernel/random/uuid)
+mkdir -p /tmp/deploy/$UUID
+cp -r ../src /tmp/deploy/$UUID
+cd /tmp/deploy
+tar -czf $UUID.tar.gz $UUID
 
 for ip in "${array[@]}"
 do	
 	echo $ip
-	scp src.tar.gz  geko@$ip:
-	ssh geko@$ip /home/geko/run_match.sh >/tmp/deploy/$ip.log &
+	scp /tmp/deploy/$UUID.tar.gz geko@$ip:test
 done
 exit 0
+
+
+
+
+
