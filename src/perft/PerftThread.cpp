b8eae174e1e2822c3453fcb625aafce5c2f5c131
geko <geko@geko-HP-Compaq-6005-Pro-SFF-PC.seat.it>
2015-09-25 10:02:04 +0200

    .

--- src/perft/PerftThread.cpp
+++ src/perft/PerftThread.cpp
@@ -108,7 +108,7 @@ void PerftThread::search(_TsubRes &n_perft, const int depthx, const u64 nCapture
 
         int isCheck = 0;
         if (!(move->type & 0xc)) {
-            if (side == WHITE) {//TODO lento
+            if (side == WHITE) {
                 if (inCheck<WHITE>(move->from, move->to, move->type, move->pieceFrom, move->capturedPiece, move->promotionPiece)) {
                     isCheck = 1;
                 }
