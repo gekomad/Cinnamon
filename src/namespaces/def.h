43e3dd59a72d1d5c837661553c1449d191267c16
gekomad <info@cinnamon.com>
2015-09-27 12:17:39 +0200

    .

---
+++ src/namespaces/def.h
@@ -0,0 +1,60 @@
+/*
+    Cinnamon UCI chess engine
+    Copyright (C) Giuseppe Cannella
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#pragma once
+
+#include "../util/stacktrace.h"
+#include "../util/Time.h"
+#include "../util/FileUtil.h"
+
+namespace _def {
+    using namespace std;
+
+    typedef unsigned char uchar;
+    typedef long long unsigned u64;
+    typedef u64 _Tchessboard[16];
+
+#if defined(CLOP) || defined(DEBUG_MODE)
+#define STATIC_CONST
+#else
+#define STATIC_CONST static const
+#endif
+
+#if defined(_MSC_VER)
+#define FORCEINLINE __forceinline
+#else
+#define FORCEINLINE __always_inline
+#endif
+
+#define assert(a) if(!(a)){  print_stacktrace();cout<<dec<<endl<<Time::getLocalTime()<<" ********************************** assert error in "<<FileUtil::getFileName(__FILE__)<< " line "<<__LINE__<<" "<<" **********************************"<<endl;cerr<<flush;exit(1);};
+
+#ifdef DEBUG_MODE
+#define ASSERT(a) assert(a)
+#define ASSERT_RANGE(value, from, to) {if ((value)<(from) || (value)>(to)){cout<<"VALUE: "<<value<<endl;assert(0)};}
+#define INC(a) (a++)
+#define ADD(a, b) (a+=(b))
+#else
+
+#define ASSERT(a)
+#define ASSERT_RANGE(value, from, to)
+#define INC(a)
+#define ADD(a, b)
+
+#endif
+}
---
+++ src/namespaces/eval.h
@@ -0,0 +1,34 @@
+/*
+    Cinnamon UCI chess engine
+    Copyright (C) Giuseppe Cannella
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+namespace _eval {
+
+    static const int VALUE_KNOWN_WIN = 15000;    //TODO stockfish
+    static const int penaltyKRKN[8] = {0, 10, 14, 20, 30, 42, 58, 80};    //TODO stockfish
+    static const int KBNKMateTable[64] = {    //TODO stockfish
+            200, 190, 180, 170, 170, 180, 190, 200, 190, 180, 170, 160, 160, 170, 180, 190, 180, 170, 155, 140, 140, 155, 170, 180, 170, 160, 140, 120, 120, 140, 160, 170, 170, 160, 140, 120, 120, 140, 160, 170, 180, 170, 155, 140, 140, 155, 170, 180, 190, 180, 170, 160, 160, 170, 180, 190, 200, 190, 180, 170, 170, 180, 190, 200};
+
+    static const int MateTable[64] = {    //TODO stockfish
+            100, 90, 80, 70, 70, 80, 90, 100, 90, 70, 60, 50, 50, 60, 70, 90, 80, 60, 40, 30, 30, 40, 60, 80, 70, 50, 30, 20, 20, 30, 50, 70, 70, 50, 30, 20, 20, 30, 50, 70, 80, 60, 40, 30, 30, 40, 60, 80, 90, 70, 60, 50, 50, 60, 70, 90, 100, 90, 80, 70, 70, 80, 90, 100,};
+
+#include "move_ordering.inc"
+
+    static const int DistanceBonus[8] = {0, 0, 100, 80, 60, 40, 20, 10};    //TODO stockfish
+}
