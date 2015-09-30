f91a83f9fb83fe32e9a1cc5809eb8a9959b22ec5
geko <geko@geko-HP-Compaq-6005-Pro-SFF-PC.seat.it>
2015-09-30 14:22:20 +0200

    .

--- src/perft.sh
+++ src/perft.sh
@@ -1,6 +1,10 @@
-mkdir /home/geko/perft
-./cinnamon -perft -c2 -h5000 -d9 -F/home/geko/perft/pos1.bin >>/home/geko/perft/pos1.log
-./cinnamon -perft -c2 -h5000 -d10 -F/home/geko/perft/pos1.bin >>/home/geko/perft/pos1.log
-./cinnamon -perft -c2 -h5000 -d11 -F/home/geko/perft/pos1.bin>>/home/geko/perft/pos1.log
-./cinnamon -perft -f"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 25" -c2 -h5000 -d8 -F/home/geko/perft/pos2.bin >>/home/geko/perft/pos2.log
-./cinnamon -perft -f"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 25" -c2 -h5000 -d9 -F/home/geko/perft/pos2.bin >>/home/geko/perft/pos2.log
+engine=./cinnamon
+dir_log=/home/geko/perft
+
+mkdir -p $dir_log
+
+$engine -perft -c2 -h5000 -d9 -F$dir_log/pos1.bin 2>&1 |tee -a $dir_log/pos1.log
+$engine -perft -c2 -h5000 -d10 -F$dir_log/pos1.bin 2>&1 |tee -a  $dir_log/pos1.log
+$engine -perft -c2 -h5000 -d11 -F$dir_log/pos1.bin 2>&1 |tee -a  $dir_log/pos1.log
+$engine -perft -f"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 25" -c2 -h5000 -d8 -F$dir_log/pos2.bin 2>&1 |tee -a  $dir_log/pos2.log
+$engine -perft -f"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 25" -c2 -h5000 -d9 -F$dir_log/pos2.bin 2>&1 |tee -a  $dir_log/pos2.log
