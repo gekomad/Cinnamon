55781b4517d3eee0ec5ea9b9d9a3abf59bbe3dd9
gekomad <info@cinnamon.com>
2015-10-07 23:20:22 +0200

    .

---
+++ src/network/util/Network.h
@@ -0,0 +1,59 @@
+/*
+    Cinnamon UCI chess engine
+    Copyright (C) Giuseppe Cannella
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h> /* for strncpy */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+class Network {
+public:
+    static string getIp() {
+
+        int fd;
+        struct ifreq ifr;
+
+        fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+        ifr.ifr_addr.sa_family = AF_INET;
+
+
+        strncpy(ifr.ifr_name, "eth0", IFNAMSIZ - 1);
+
+        ioctl(fd, SIOCGIFADDR, &ifr);
+
+        close(fd);
+
+        return inet_ntoa(((struct sockaddr_in *) &ifr.ifr_addr)->sin_addr);
+
+
+    }
+};
+
