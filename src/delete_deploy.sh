209921db1f57bb0eb1fd1c4b6ad0a18e3cce8283
gekomad <info@cinnamon.com>
2016-02-06 09:45:00 +0100

    .

---
+++ src/delete_deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+~/workspace/assembla_git/chess-utility/script/remote/bash.sh "/home/geko/Kill.sh"
+~/workspace/assembla_git/chess-utility/script/remote/bash.sh "rm -fr test"
+
+./deploy.sh
+exit 0
+ 
+
+
