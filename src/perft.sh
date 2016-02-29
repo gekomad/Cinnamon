21d308de341cdaa59f46ef9bf50da4b53b723f91
geko <geko@geko.geko>
2016-02-29 12:21:44 +0100

    .

--- src/perft.sh
+++
@@ -1,9 +0,0 @@
-engine=/home/geko/chess/engines/cinnamon12c_16
-dir_log=/home/geko/perft
-
-mkdir -p $dir_log
-
-$engine -perft -f"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 25" -c2 -h5000 -d8 -F$dir_log/pos2.bin 2>&1 |tee -a  $dir_log/pos2.log
-$engine -perft -c2 -h5000 -d10 -F$dir_log/pos1.bin 2>&1 |tee -a  $dir_log/pos1.log
-$engine -perft -c2 -h5000 -d11 -F$dir_log/pos1.bin 2>&1 |tee -a  $dir_log/pos1.log
-$engine -perft -f"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 25" -c2 -h5000 -d9 -F$dir_log/pos2.bin 2>&1 |tee -a  $dir_log/pos2.log
