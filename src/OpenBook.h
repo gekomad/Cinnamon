0694eaf282385a09c3735a8a3f29bc917e94dc29
geko <geko@geko.geko>
2016-03-10 11:30:57 +0100

    .

--- src/OpenBook.h
+++ src/OpenBook.h
@@ -21,9 +21,9 @@
 #include <fstream>
 #include "Eval.h"
 
-#if defined(_WIN32)
-#include "util/Time.h"
-#endif
+//#if defined(_WIN32)
+//#include "util/Time.h"
+//#endif
 
 class OpenBook : public Singleton<OpenBook> {
     friend class Singleton<OpenBook>;
