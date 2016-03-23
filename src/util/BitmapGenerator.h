102409f49e871b40f5d74f90b029c4a0704de136
gekomad <info@cinnamon.com>
2016-03-23 21:10:45 +0100

    .

--- src/util/BitmapGenerator.h
+++ src/util/BitmapGenerator.h
@@ -19,11 +19,11 @@ public:
     static u64 BITMAP_SHIFT_COLUMN[64][256];
     static u64 BITMAP_SHIFT_RANK[64][256];
 
-#define diagonalIdx(position, allpieces) ( (uchar)((( _board::LEFT_DIAG[position]  & (allpieces)) * BitmapGenerator::MAGIC_KEY) >> 56   ))
+#define diagonalIdx(position, allpieces) ( (uchar)((( (allpieces) & _board::LEFT_DIAG[position] ) * BitmapGenerator::MAGIC_KEY) >> 56 ))
 
-#define antiDiagonalIdx(position, allpieces) ((uchar) ((( _board::RIGHT_DIAG[position]  & (allpieces)) * BitmapGenerator::MAGIC_KEY) >> 56))
+#define antiDiagonalIdx(position, allpieces) ((uchar) ((( (allpieces) & _board::RIGHT_DIAG[position] ) * BitmapGenerator::MAGIC_KEY) >> 56))
 
-#define columnIdx(position, allpieces) ((uchar)(((allpieces & FILE_[position]) * BitmapGenerator::MAGIC_KEY_FILE) >>56))
+#define columnIdx(position, allpieces) ((uchar)((((allpieces) & FILE_[position]) * BitmapGenerator::MAGIC_KEY_FILE) >>56))
 
 #define rankIdx(position, allpieces) ((uchar)((allpieces) >> RANK_ATx8[position]))
 
