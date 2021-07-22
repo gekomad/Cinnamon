3add27bbcf7a321d309711ae917ae2001f8a52b2
gekomad <giuseppe.cannella@gmail.com>
2021-07-22 14:34:53 +0200

    1

--- src/Search.cpp
+++ src/Search.cpp
@@ -421,8 +421,8 @@ int Search::search(const int depth, int alpha, const int beta, _TpvLine *pline, 
             }
             //Late Move Reduction
             if (countMove > 3 && !isIncheckSide && depth >= 3 && move->capturedPiece == SQUARE_EMPTY) {
-               // cout <<depth<<" "<<countMove<<"    "<< (countMove > 6 ? 3 : 2)<<" "<<LMR[depth][countMove]<< endl;
-                val = searchLambda(&newLine, depth + extension - LMR[depth][countMove], -(alpha + 1), -alpha,
+//                cout <<depth<<" "<<countMove<<"    "<< (countMove > 6 ? 3 : 2)<<" "<<(1.8865 *log(countMove))<< endl;
+                val = searchLambda(&newLine, depth + extension - (1.8865 *log(countMove)), -(alpha + 1), -alpha,
                                    nullptr);
             }
         }
